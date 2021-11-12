f351e5086e46e6096f9ae392f66f492b750a6601
Sayed-Anawer <sayedanawer99@gmail.com>
2021-11-12 18:27:17 +0600

    first commit

---
+++ src/Components/Dashboard/AddReview/AddReview.js
@@ -0,0 +1,93 @@
+import axios from "axios";
+import React from "react";
+import { useForm } from "react-hook-form";
+const AddReview = () => {
+  const { register, handleSubmit, reset } = useForm();
+  document.title = "Add More Trips";
+
+  const onSubmit = (data) => {
+    axios
+      .post("https://arcane-peak-21353.herokuapp.com/reviews", data)
+      .then((res) => {
+        if (res.data.insertedId) {
+          alert("Your Review has been  Added");
+          reset();
+        }
+      });
+    console.log(data);
+  };
+  return (
+    <div
+      className="container mt-5"
+      style={{ height: "100vh", marginBottom: "200px" }}
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label for="exampleInputName" className="form-label">
+            Name of User
+          </label>
+          <input
+            className="form-control"
+            id="exampleInputName"
+            {...register("name")}
+          />
+        </div>
+        <p>Give us Rate</p>
+        {/* rating */}
+        {[1, 2, 3, 4, 5].map((e, index) => {
+          return (
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="inlineRadioOptions"
+                id={`inlineRadio${index + 1}`}
+                value={index + 1}
+                {...register("rating")}
+              />
+              <label
+                className="form-check-label"
+                for={`inlineRadio${index + 1}`}
+              >
+                {index + 1}
+              </label>
+            </div>
+          );
+        })}
+        <br />
+        <br />
+        <div className="mb-3">
+          <label for="exampleInputReview" className="form-label">
+            Write your Review
+          </label>
+          <textarea
+            cols="10"
+            rows="10"
+            className="form-control"
+            id="exampleInputReview"
+            {...register("review")}
+          />
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputImg" className="form-label">
+            Image Url
+          </label>
+          <input
+            className="form-control"
+            id="exampleInputImg"
+            {...register("img")}
+          />
+        </div>
+
+        <input
+          //   className="btn btn-primary"
+          type="submit"
+          className="btn btn-dark"
+          value="Add a review"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default AddReview;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Banner2 from "../Banner2/Banner2";
+import Reviews from "../Reviews/Reviews";
+import Footer from "../Shared/Footer/Footer";
+import Navbar from "../Shared/Navbar/Navbar";
+import Subscribe from "../Subscribe/Subscribe";
+
+const Home = () => {
+  const [cars, setCars] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://arcane-peak-21353.herokuapp.com/allCars")
+
+      .then((res) => setCars(res.data.slice(0, 6)));
+  }, []);
+  return (
+    <>
+      <div style={{ marginBottom: "370px" }}>
+        <Navbar />
+
+        <Banner2 />
+        <div className="container my-5">
+          <h2 className="text-center  ">
+            The <span className="text-danger "> Latest </span> New car{" "}
+          </h2>
+          <div className="row row-cols-1 row-cols-md-3 g-4 ">
+            {cars.map((car) => {
+              return (
+                <div className="col " key={car._id}>
+                  <div className="card h-100">
+                    <img
+                      src={car.img}
+                      className="img-fluid card-img-top h-100 "
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <h4 className="card-title">{car.name}</h4>
+                      <h5 className="card-title my-4">Price : ${car.price}</h5>
+                      <p className="card-text">{car.desc}</p>
+                      <Link to={`/purchase/${car._id}`}>
+                        <button className="w-100 btn btn-dark">
+                          Purchase now
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <Reviews />
+        <Subscribe />
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
